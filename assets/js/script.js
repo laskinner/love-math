c15a68a5b1140f6da3d016513efe182519a8ef11
Luke Skinner <laskinner@gmail.com>
2023-10-25 13:36:53 +0200

    Added final tidying up changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
         runGame(gameType);
       }
     });
-  }
+  } DocumentDocumentDocumentDocumentDocumentDocumentDocumentDocumentDocumentDocumentDocument
 
   runGame("addition");
 
@@ -24,6 +24,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+  document.getElementById("answer-box").value = "";
+  document.getElementById("answer-box").focus();
 
   // Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
@@ -57,6 +60,11 @@ function checkAnswer() {
     incrementWrongAnswer();
   }
 
+  document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  })
   runGame(calculatedAnswer[1]);
 
 }
